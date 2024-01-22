52a7a7025dfac574c83521dfa7d382bd4ed049b8
Jimmy <skawk3235@gmail.com>
2024-01-22 15:55:30 +0900

    js - 내장객체 (date)

---
+++ js_study2/01_22/implicit_object.js
@@ -0,0 +1,15 @@
+var now = new Date();
+console.log(Date());
+console.log(new Date());
+
+var date = now.getDate();
+var hour = now.getHours();
+
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+console.log(now.getMilliseconds());
+console.log(now.toLocaleString('ko-KR'));
