0deb99c074612339359bf78a79cedc074f38bcc0
Jimmy <skawk3235@gmail.com>
2024-02-01 17:52:01 +0900

    swiper__content__date 날짜 받아와서 써지도록 변경

--- mini_project_02/src/swiper.js
+++ mini_project_02/src/swiper.js
@@ -1,10 +1,13 @@
+'use strict';
+
 import { data } from './swiper_data.js';
 
 const dataArr = data.contents;
 const swiperColor = ['#FFFFF3', '#B8E5E0', '#E1F5FC', '#FFFFFF', '#F8E1B7'];
-console.log(dataArr);
 
 const swiperContents = document.querySelectorAll('.swiper__content');
+const swiperContentDate = document.querySelectorAll('.swiper__content__date');
+
 swiperContents.forEach((item, index) => {
   const img = document.createElement('img');
   img.setAttribute('class', 'swiper__content__img');
@@ -12,6 +15,15 @@ swiperContents.forEach((item, index) => {
   img.setAttribute('alt', `${dataArr[index].MINCLASSNM}`);
   item.style.backgroundColor = swiperColor[index];
   item.appendChild(img);
+
+  item.addEventListener('click', () => {
+    location.href = `./html/details.html?INDEX=${dataArr[index].INDEX}`;
+  });
+
+  swiperContentDate[index].innerHTML = `${dataArr[index].SVCOPNBGNDT.slice(
+    0,
+    11
+  )} ~ ${dataArr[index].SVCOPNENDDT.slice(0, 11)}`;
 });
 
 const swiper = new Swiper('.mySwiper', {
