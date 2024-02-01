11ce9b06a3f38a34b9fddbc00d4bf44070a0970b
Jimmy <skawk3235@gmail.com>
2024-02-01 16:31:56 +0900

    swiper관련 소스를 swiper_data로 변경 후 swiper로 이동

--- mini_project_02/src/swiper.js
+++ mini_project_02/src/swiper.js
@@ -1,4 +1,20 @@
-var swiper = new Swiper('.mySwiper', {
+import { data } from './swiper_data.js';
+
+const dataArr = data.contents;
+const swiperColor = ['#FFFFF3', '#B8E5E0', '#E1F5FC', '#FFFFFF', '#F8E1B7'];
+console.log(dataArr);
+
+const swiperContents = document.querySelectorAll('.swiper__content');
+swiperContents.forEach((item, index) => {
+  const img = document.createElement('img');
+  img.setAttribute('class', 'swiper__content__img');
+  img.setAttribute('src', `${dataArr[index].IMGURL}`);
+  img.setAttribute('alt', `${dataArr[index].MINCLASSNM}`);
+  item.style.backgroundColor = swiperColor[index];
+  item.appendChild(img);
+});
+
+const swiper = new Swiper('.mySwiper', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
