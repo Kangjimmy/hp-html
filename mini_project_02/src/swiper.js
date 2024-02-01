de925c8c6a0d47f8cb5a9ca0905d0d109d33d459
Jimmy <skawk3235@gmail.com>
2024-02-01 09:18:00 +0900

    miniproject02 - html, css

---
+++ mini_project_02/src/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
