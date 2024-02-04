5472616d4d67c8c755250e0d2ae5c10a973d7b09
jimmy <skawk3235@gmail.com>
2024-02-04 13:54:51 +0900

    행사 위치 지도에 표시되도록 수정

--- mini_project_02/src/details.js
+++ mini_project_02/src/details.js
@@ -16,6 +16,7 @@ const liSvcopn = document.querySelector('.li__svcopn');
 const liPlacenm = document.querySelector('.li__placenm');
 const liUsetgtinfo = document.querySelector('.li__usetgtinfo');
 const liPayatnm = document.querySelector('.li__payatnm');
+const detailsBtnLink = document.querySelector('.details__btn__link > a');
 
 const [SVCOPN, PLACENM, USETGTINFO, PAYATNM] = [
   `${indexData.SVCOPNBGNDT.substr(0, 10)} ~ ${indexData.SVCOPNENDDT.substr(
@@ -31,6 +32,28 @@ liPlacenm.innerHTML = PLACENM;
 liUsetgtinfo.innerHTML = USETGTINFO;
 liPayatnm.innerHTML = PAYATNM;
 
+detailsBtnLink.setAttribute('href', indexData.SVCURL);
+
 function getIndexData(dataArr, index) {
   return dataArr.find((item) => item.INDEX == index);
 }
+
+const mapContainer = document.getElementById('map'), // 지도를 표시할 div
+  mapOption = {
+    center: new kakao.maps.LatLng(indexData.Y, indexData.X), // 지도의 중심좌표
+    level: 3, // 지도의 확대 레벨
+  };
+
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+const map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 마커가 표시될 위치입니다
+const markerPosition = new kakao.maps.LatLng(indexData.Y, indexData.X);
+
+// 마커를 생성합니다
+const marker = new kakao.maps.Marker({
+  position: markerPosition,
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
