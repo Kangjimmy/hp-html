9eada31c9743c199779cee63f7653be5be45c671
jimmy <skawk3235@gmail.com>
2024-02-04 13:31:26 +0900

    세부목록 정보 jsonData에서 받아와 보여주기

--- mini_project_02/src/details.js
+++ mini_project_02/src/details.js
@@ -1,11 +1,36 @@
 'use strict';
-import { jsonData } from './json_data_index.js';
+import { getManagedData } from './common.js';
 
 const urlParams = new URLSearchParams(window.location.search);
-console.log(urlParams.get('INDEX'));
+const jsonData = getManagedData();
 let index = urlParams.get('INDEX');
-console.log(jsonData[index]);
+const indexData = getIndexData(jsonData, index);
+console.log(indexData);
 
 const detailsImg = document.querySelector('.details__img');
+detailsImg.setAttribute('src', indexData.IMGURL);
 
-detailsImg.setAttribute('src', jsonData[index].IMGURL);
+const liSvcnm = document.querySelector('.li__svcnm');
+liSvcnm.innerHTML = indexData.SVCNM;
+const liSvcopn = document.querySelector('.li__svcopn');
+const liPlacenm = document.querySelector('.li__placenm');
+const liUsetgtinfo = document.querySelector('.li__usetgtinfo');
+const liPayatnm = document.querySelector('.li__payatnm');
+
+const [SVCOPN, PLACENM, USETGTINFO, PAYATNM] = [
+  `${indexData.SVCOPNBGNDT.substr(0, 10)} ~ ${indexData.SVCOPNENDDT.substr(
+    0,
+    10
+  )}`,
+  indexData.PLACENM,
+  indexData.USETGTINFO,
+  indexData.PAYATNM,
+];
+liSvcopn.innerHTML = SVCOPN;
+liPlacenm.innerHTML = PLACENM;
+liUsetgtinfo.innerHTML = USETGTINFO;
+liPayatnm.innerHTML = PAYATNM;
+
+function getIndexData(dataArr, index) {
+  return dataArr.find((item) => item.INDEX == index);
+}
