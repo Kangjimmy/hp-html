0aaa2f0900b29ed74a28462d4d7fa9738aa5359c
Jimmy <skawk3235@gmail.com>
2024-03-20 14:10:12 +0900

    eventApp으로 변경

---
+++ react/react-app/src/EventHookApp.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import AddName from './components/09.eventAndHooks/AddName';
+import PreventDefaultClick from './components/09.eventAndHooks/PreventDefaultClick';
+import ChangeFont from './components/09.eventAndHooks/ChangeFont';
+import EventOnChange from './components/09.eventAndHooks/EventOnChange';
+import MultiInput from './components/09.eventAndHooks/MultiInput';
+import NameForm from './components/09.eventAndHooks/NameForm';
+import TextArea from './components/09.eventAndHooks/TextArea';
+
+export default function HookApp() {
+  return (
+    <div>
+      {/* <AddName /> */}
+      {/* <PreventDefaultClick /> */}
+      {/* <ChangeFont /> */}
+      {/* <EventOnChange /> */}
+      {/* <MultiInput /> */}
+      {/* <NameForm /> */}
+      <TextArea />
+    </div>
+  );
+}
--- react/react-app/src/index.js
+++ react/react-app/src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import StateApp from './StateApp';
+import EventHookApp from './EventHookApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>
-  <StateApp />
+  // <StateApp />
+  <EventHookApp />
 );
 
 // If you want to start measuring performance in your app, pass a function
