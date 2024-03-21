e4749e0ea9d7146981f670a5418313d8b3404533
Jimmy <skawk3235@gmail.com>
2024-03-21 15:17:56 +0900

    post css

--- react/react-app/src/components/08.componentStyling/Button1.jsx
+++ react/react-app/src/components/08.componentStyling/Button1.jsx
@@ -3,5 +3,10 @@ import React from 'react';
 import styles from './PostButton1.module.css';
 
 export default function Button1() {
-  return <button className={styles.button}>버튼1</button>;
+  return (
+    <div>
+      <div className={styles.pic}>a</div>
+      <button className={styles.button}>버튼1</button>
+    </div>
+  );
 }
